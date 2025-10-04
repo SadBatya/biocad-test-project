5fac8e2339d4e1e25a569ac5bd9831cd67230f67
sadbatya <sadbatya@mail.ru>
2025-10-04 12:24:07 +0200

    refactor: удалил ненужные шрифты

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Lato } from "next/font/google";
+import { Lato } from "next/font/google";
 import "./globals.css";
 import { Header, Footer } from "@/widgets";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const lato = Lato({
   variable: "--font-lato",
@@ -30,10 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} ${lato.variable}`}
-      >
+    <html lang="ru">
+      <body className={`${lato.variable}`}>
         <Header />
         {children}
         <Footer />
